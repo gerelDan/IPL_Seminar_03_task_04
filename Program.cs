9c03997bec4c42d68007db536b4ad7571b1be24f
gerel <gerel@bk.ru>
2023-12-12 22:29:32 +0500

    Final_v2

--- Program.cs
+++ Program.cs
@@ -15,3 +15,20 @@ while (number/10 != 0)
 result[result.Length-1] = number;
 Array.Reverse(result);
 Console.WriteLine($"Результат: [{string.Join(", ", result)}]");
+//Если реверс это тоже читерство то можно так:
+int number2 = new Random().Next(1, 10_000);
+Console.WriteLine($"Число: {number2}");
+int[] resultSecond = new int[1];
+while (number2/10 != 0)
+{
+    resultSecond[resultSecond.Length-1] = number2 % 10;
+    number2 /= 10;
+    Array.Resize(ref resultSecond, resultSecond.Length + 1);
+}
+resultSecond[resultSecond.Length-1] = number2;
+int[] resultFinal = new int[resultSecond.Length];
+for (int i=0; i<resultFinal.Length; i++)
+{
+    resultFinal[i] = resultSecond[resultSecond.Length-i-1];
+}
+Console.WriteLine($"Результат: [{string.Join(", ", resultFinal)}]");
